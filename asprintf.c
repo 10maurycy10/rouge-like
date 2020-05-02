54244ba030e7a23a2f66ff84a3d6b22c6ce73113
10maurycy10 <10maurycy10@users.noreply.github.com>
2020-05-02 10:30:57 -0800

    fixes

--- asprintf.c
+++ asprintf.c
@@ -7,10 +7,12 @@ int asprintf(char** dest, char* fmt, ...) {
     va_list args;
     va_start(args,256);
     va_list args2;
-    va_list args3;
     va_copy(args2,args);
-    *dest = malloc(1024);//_vscprintf(fmt,_VA_LIST args2)); //TODO THIS BUFFER
-    va_copy(args3,args);
-    sprintf(*dest,fmt, _VA_LIST args3);
+    *dest = malloc(vsnprintf(NULL, 0, fmt, args2) + 1);
+    va_end(args2);
+    va_copy(args2,args);
+    vsprintf(*dest,fmt, args2);
+    va_end(args2);
+    va_end(args);
     return 0;
 }
